792e6add61fd846b2c337dc7055297bf41f25a5c
Febriandianto <prabowo.febrian@gmail.com>
2023-03-31 10:35:46 +0700

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by febriandianto prabowo on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
