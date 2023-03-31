13310ff09ad5f67eddabffd7ae6b4e99833730ea
Febriandianto <prabowo.febrian@gmail.com>
2023-03-31 10:43:17 +0700

    Uji coba2

--- Testing/ContentView.swift
+++ Testing/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Testing")
+            Text("Hello, Testing2")
         }
         .padding()
     }
