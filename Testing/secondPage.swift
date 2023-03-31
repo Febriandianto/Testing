e5264747ac2e48bed676b3daac39ba1a697fa880
Febriandianto <prabowo.febrian@gmail.com>
2023-03-31 10:50:03 +0700

    create second view

---
+++ Testing/secondPage.swift
@@ -0,0 +1,20 @@
+//
+//  secondPage.swift
+//  Testing
+//
+//  Created by febriandianto prabowo on 31/03/23.
+//
+
+import SwiftUI
+
+struct secondPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct secondPage_Previews: PreviewProvider {
+    static var previews: some View {
+        secondPage()
+    }
+}
